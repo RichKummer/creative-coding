496953a7a5d4acde2592a0d68065fa053bd082db
RichKummer <rckummer@gmail.com>
2022-04-03 13:52:58 -0400

    Arcs

--- sketches/sketch-02.js
+++ sketches/sketch-02.js
@@ -6,7 +6,8 @@ const settings = {
   dimensions: [ 1080, 1080 ]
 };
 
-// change radians to degrees
+// change radians to degrees. Added utility above so these two aren't needed
+/*
 const degToRad = (degrees) => {
   return degrees / 180 * Math.PI;
 };
@@ -14,6 +15,7 @@ const degToRad = (degrees) => {
 const randomRange = (min, max) => {
   return Math.random() * (max - min) + min;
 };
+*/
 
 const sketch = () => {
   return ({ context, width, height }) => {
@@ -28,7 +30,7 @@ const sketch = () => {
     const h = height * 0.1;
     let x, y;
 
-    const num = 12;
+    const num = 40;
     const radius = width * 0.3;
 
     for (let i = 0; i < num; i++) {
@@ -42,12 +44,23 @@ const sketch = () => {
       context.save();
       context.translate(x, y);
       context.rotate(-angle);
-      context.scale(random.range(1, 3), 1);
+      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
 
       
       context.beginPath();
-      context.rect( -w * 0.5, -h * 0.5, w, h);
+      context.rect( -w * 0.5, random.range(0, -h * 0.5), w, h);
       context.fill();
+      context.restore();
+
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(5, 20);
+      
+      context.beginPath();
+      context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(1, -8), slice * random.range(1, 5));
+      context.stroke();
 
       context.restore();
     }
